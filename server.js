fe64f9475f64cf7876ba4149fdfd5e2889e99c00
Abhishekji-Dumala <30731706+Abhishekji-Dumala@users.noreply.github.com>
2017-08-09 20:05:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-two', function (req, res){
     res.send('Article two is requested and will be served here');
 });
 app.get('/article-three', function (req, res){
-    res.send('Article two is requested and will be served here');
+    res.send('Article three is requested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
