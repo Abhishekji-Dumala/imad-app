f792260ab3f558d8b532ac984015881d8e6eb9cd
Abhishekji-Dumala <30731706+Abhishekji-Dumala@users.noreply.github.com>
2017-08-10 09:43:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New valu';
 
 //Move the image
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginLeft = '100px';
 };
